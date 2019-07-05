f7e2ace886b506305dc5baf678729bb0c69a6b50
Lyudmyla Lyashuk <lyudmyla.horbyk@gmail.com>
2019-07-05 20:18:28 +0300

    Update task01.js

--- task01.js
+++ task01.js
@@ -1,4 +1,4 @@
-// Task 1
+// Task 1 - https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript?fbclid=IwAR0SNApzz2GCz-cLVz-Aro6t4gDBR3SZVvP64zvNwmOFlisdHkCZpCLKekY
 
 function circleCircumference(circle) {
   const circumference = circle.radius * 2 * Math.PI;
