3919e123cfaf127df4ec49b1d73aec729dc101ae
Lyudmyla Lyashuk <lyudmyla.horbyk@gmail.com>
2019-07-05 20:19:17 +0300

    Update task03.js

--- task03.js
+++ task03.js
@@ -1,4 +1,4 @@
-// Task 3
+// Task 3 - https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript?fbclid=IwAR0cz-bnHFpnEbr1zBd_sbyaLoHQ1_s461cAM8aORQFhEjoFv9IeWDO7CNw
 
 function buildFun(n) {
   var res = [];
